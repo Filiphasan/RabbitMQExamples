7c5d67cd4e6144b4880adfbb9ee07393e2fba9fa
Hasan Erdal <hasaerda@hotmail.com>
2022-08-28 13:13:33 +0300

    basic usage example of rabbitmq

---
+++ BasicUsageRabbitMQ/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri("your connection string");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+string queName = "first-que";
+
+channel.QueueDeclare(queName, false, false, false);
+
+string queMessage = "Hello World!";
+
+var queBytes = Encoding.UTF8.GetBytes(queMessage);
+
+channel.BasicPublish(string.Empty, queName, null, queBytes);
+
+
+Console.WriteLine("Mesaj Kuyruğa iletilmiştir.");
+
+Console.ReadKey();
+
---
+++ Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri("your connection string");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+string queName = "first-que";
+
+//channel.QueueDeclare(queName, false, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(queName, true, consumer);
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Console.WriteLine(message);
+};
+
+Console.ReadKey();
+
