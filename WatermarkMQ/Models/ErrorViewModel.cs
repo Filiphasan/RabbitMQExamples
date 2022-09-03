99c6a602b40d6351650033fa15e70aa99b3b57e8
Hasan Erdal <hasaerda@hotmail.com>
2022-09-03 11:43:34 +0300

    Create watermark aspnet core mvc project and add install need package

---
+++ WatermarkMQ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WatermarkMQ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
